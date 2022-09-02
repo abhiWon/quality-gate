5dada5da5693852b4b028e381e14add1c949df44
abhiWon <allstate091092@gmail.com>
2022-09-02 16:21:54 +0530

    Sonar Cloud Fixes

--- app/factorialUsingRecursion.js
+++ app/factorialUsingRecursion.js
@@ -10,39 +10,39 @@ require([
   Font, TextSymbol, Extent, webMercatorUtils, arrayUtils, Color, parser, BootstrapMap
 ) {
     parser.parse();
-    var map = new Map("mapDiv", { center: [-56.049, 38.485], zoom: 3, basemap: "hybrid" });
+    let map = new Map("mapDiv", { center: [-56.049, 38.485], zoom: 3, basemap: "hybrid" });
     BootstrapMap.bindTo(map);
     locator = new Locator("https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer");
     locator.on("address-to-locations-complete", function (evt) {
         map.graphics.clear();
         arrayUtils.forEach(evt.addresses, function (geocodeResult, index) {
-            var r = Math.floor(Math.random() * 250);
-            var g = Math.floor(Math.random() * 100);
-            var b = Math.floor(Math.random() * 100);
+            let r = Math.floor(Math.random() * 250);
+            let g = Math.floor(Math.random() * 100);
+            let b = Math.floor(Math.random() * 100);
 
-            var symbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.STYLE_CIRCLE, 20, new SimpleLineSymbol(
+            let symbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.STYLE_CIRCLE, 20, new SimpleLineSymbol(
                 SimpleLineSymbol.STYLE_SOLID, new Color([r, g, b, 0.5]), 10), new Color([r, g, b, 0.9]));
 
-            var pointMeters = webMercatorUtils.geographicToWebMercator(geocodeResult.location);
-            var locationGraphic = new Graphic(pointMeters, symbol);
-            var font = new Font().setSize("12pt").setWeight(Font.WEIGHT_BOLD);
-            var textSymbol = new TextSymbol( (index + 1) + ".) " + geocodeResult.address, font, new Color([r, g, b, 0.8]) ).setOffset(5, 15);
+            let pointMeters = webMercatorUtils.geographicToWebMercator(geocodeResult.location);
+            let locationGraphic = new Graphic(pointMeters, symbol);
+            let font = new Font().setSize("12pt").setWeight(Font.WEIGHT_BOLD);
+            let textSymbol = new TextSymbol( (index + 1) + ".) " + geocodeResult.address, font, new Color([r, g, b, 0.8]) ).setOffset(5, 15);
             map.graphics.add(locationGraphic);
             map.graphics.add(new Graphic(pointMeters, textSymbol));
         });
-        var ptAttr = evt.addresses[0].attributes;
-        var minx = parseFloat(ptAttr.Xmin);
-        var maxx = parseFloat(ptAttr.Xmax);
-        var miny = parseFloat(ptAttr.Ymin);
-        var maxy = parseFloat(ptAttr.Ymax);
-        var esriExtent = new Extent(minx, miny, maxx, maxy, new SpatialReference({ wkid: 4326 }));
+        let ptAttr = evt.addresses[0].attributes;
+        let minx = parseFloat(ptAttr.Xmin);
+        let maxx = parseFloat(ptAttr.Xmax);
+        let miny = parseFloat(ptAttr.Ymin);
+        let maxy = parseFloat(ptAttr.Ymax);
+        let esriExtent = new Extent(minx, miny, maxx, maxy, new SpatialReference({ wkid: 4326 }));
         map.setExtent(webMercatorUtils.geographicToWebMercator(esriExtent));
     });
     //-- Wire up annyang 
     if (annyang) {
         annyang.debug()
         $('#welcome').fadeIn('fast');
-        var move = function (dir) {
+        let move = function (dir) {
             if (dir == "left" || dir == "west") map.panLeft();
             if (dir == "right" || dir == "east") map.panRight();
             if (dir == "up" || dir == "north") map.panUp();
@@ -50,18 +50,18 @@ require([
             if (dir == "in") map.setZoom(map.getZoom() + 1);
             if (dir == "out") map.setZoom(map.getZoom() - 1);
         };
-        var setBasemap = function (basemap) {
+        let setBasemap = function (basemap) {
             if (basemap == 'gray') basemap = 'grey'; //helper
             if (basemap == 'street') basemap = 'streets'; //helper
-            var baseMaps = $("a[data-basemapname='" + basemap + "']");
+            let baseMaps = $("a[data-basemapname='" + basemap + "']");
             if (baseMaps.length > 0) map.setBasemap($(baseMaps[0]).attr("data-basemapvalue"));
         };
-        var close = function () {
+        let close = function () {
             $('#welcome').fadeOut('fast');
             $('#help').modal('hide');
         };
-        var showHelp = function () { $('#help').modal('show'); };
-        var commands = {
+        let showHelp = function () { $('#help').modal('show'); };
+        let commands = {
             'move :dir': move,
             'locate *place': geoLocate,
             'set base map *basemap': setBasemap,
@@ -80,8 +80,8 @@ require([
     }
     //-- Helper functions
     function geoLocate(place) {
-        var address = { SingleLine: place };
-        var options = { address: address, outFields: ["*"] };
+        let address = { SingleLine: place };
+        let options = { address: address, outFields: ["*"] };
         locator.addressToLocations(options);
     }
     //-- Event Binding
